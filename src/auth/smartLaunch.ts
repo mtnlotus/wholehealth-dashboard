5a72ec52e3eb9a5bf8637c799e594afc03f4f26d
Dave Carlson <dave@mtnlotus.com>
2026-05-11 13:31:48 -0400

    Use fhirServers.ts to select client_id

--- src/auth/smartLaunch.ts
+++ src/auth/smartLaunch.ts
@@ -1,8 +1,13 @@
 import FHIR from "fhirclient";
+import { clientIdForIss } from "../config/fhirServers";
 
 export async function smartLaunch(): Promise<void> {
+  const iss =
+    new URLSearchParams(window.location.search).get("iss") ??
+    import.meta.env.VITE_FHIR_ISS ??
+    "";
   await FHIR.oauth2.authorize({
-    client_id: import.meta.env.VITE_SMART_CLIENT_ID,
+    client_id: clientIdForIss(iss),
     scope: "launch patient/Patient.read patient/DocumentReference.read patient/Binary.read patient/Goal.read openid fhirUser",
     redirect_uri: `${window.location.origin}/callback`,
   });
